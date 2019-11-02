4d549e28336f227ec62d04ebe08b82d62ef9aaa6
raspi0124 <adores0124@yahoo.co.jp>
2019-11-03 02:16:31 +0900

    Implemented basic UI, and usage showing system.

---
+++ clientlib.js
@@ -0,0 +1,130 @@
+function clientupdate(name, updateto) {
+  if (name == "tu") { //Today's usage
+    var tu = document.getElementById("todayusage");
+    tu.innerHTML = "￥" + updateto + "";
+  }
+  if (name == "bpd") { //Budget per day, Likely be divided by Total balance divided by remaining number of 平日
+    var bpd = document.getElementById("budgetperday")
+    bpd.innerHTML = "￥" + updateto + "";
+  }
+	if (name == "remainingb") { //Total Balance
+		var bpd = document.getElementById("remainingbalance")
+		bpd.innerHTML = "￥" + updateto + "";
+	}
+  //if (name == "totalb") { //Total Balance
+  //  var bpd = document.getElementById("totalbalance")
+  //  bpd.innerHTML = "￥" + updateto + "";
+  //}
+}
+
+
+function sum(input){
+
+ if (toString.call(input) !== "[object Array]")
+  return false;
+
+  var total =  0;
+  for(var i=0;i<input.length;i++)
+    {
+      if(isNaN(input[i])){
+    		continue;
+      }
+      total += Number(input[i]);
+    }
+	return total;
+}
+
+var latestrecieve = 0 //初回はここでdefineすることによってundefined variableエラーを回避
+function upclientusagelog(used, reason, created_at) {
+	var timestamp = Math.floor(Date.now()/1000);
+	var x = document.getElementById("usagelog")
+	console.log("updateclientusagelog")
+	console.log(created_at)
+	var created_at = parseInt(created_at)
+	console.log(created_at)
+	csrmili = Math.floor(created_at*1000); //標準ではミリ秒ではなく秒でtimestampになっているので
+	var csr = new Date(csrmili)
+	console.log(csr)
+	var csrmonth = csr.getMonth() + 1
+	var csrdate = csr.getFullYear() + "/" + csrmonth + "/"+ csr.getDate()
+  var toadd = '<li class="list-group-item">\
+		<div class="row align-items-center no-gutters"> <div>\
+    <h6> <strong> ￥' + used + ' </strong></h6> <span class = "text-xs"> ' + reason + ', ' + csrdate + '  </span></div>\
+    </div> </li>'
+	var old = x.innerHTML
+	if (latestrecieve != timestamp){ //こうすることによって一緒の追加セッションでここに来たかそれとも別のセッション(リッスン)でここに来たかを判別。
+		//普通にelseのやつのみでやると重複が出てくるので。処理速度が遅い端末だとバグる可能性?
+		//この↓のやつだけでいいと思うかもしれないけどこれだと一つの要素しか入ってくれない。(既存の要素を新規要素受診時に消してしまう)
+		x.innerHTML = toadd
+	} else {
+		x.innerHTML = toadd + old
+	}
+	latestrecieve = timestamp
+	//x.innerHTML = toadd //本来なら↑のコメントアウトされてるやつのほうがいいんだけどsnapshotからaddする方法がうーん
+}
+
+function usagelogadder(useds, reasons, created_ats){
+	//リバースしないとめんどくさい。具体的に言うと古→新順にログが並んでしまうため一回リバース(反転)。
+	useds = useds.reverse();
+	reasons = reasons.reverse();
+	created_ats = created_ats.reverse();
+	for (var i = 0; i < useds.length; i++) { //Suppose that all the element are fullfilled
+	 console.log(useds[i])
+	 upclientusagelog(useds[i], reasons[i], created_ats[i])
+	 todayusageupdater(useds, created_ats)
+	}
+}
+
+function balanceupdater(currentbalances){
+	len = currentbalances.length
+	i = len - 1
+	clientupdate("remainingb", currentbalances[i])
+}
+
+function isittoday(timestamp){
+	var timestamp = parseInt(timestamp)
+	csrmili = Math.floor(timestamp*1000);
+	var today = new Date().setHours(0, 0, 0, 0);
+	var thatDay = new Date(csrmili).setHours(0, 0, 0, 0);
+
+	if(today === thatDay){
+    return "true";
+	}else {
+		return "false";
+	}
+}
+
+function todayusageupdater(useds, created_ats){
+	todayusage = [0]; //Initialize by having 0. This way, no undefined occur.
+	for (var i = 0; i < created_ats.length; i++) {
+	 cs = created_ats[i]
+	 used = useds[i]
+	 re = isittoday(cs)
+	 if (re=="true"){
+		 todayusage.push(used);
+	 }else {
+	 }
+	}
+	clientupdate("tu", sum(todayusage))
+}
+
+db.collection("usage").orderBy("created_at", "desc").limit(10)
+  .onSnapshot(function(querySnapshot) {
+    var useds = [];
+    var created_ats = [];
+		var currentbalances = [];
+		var reasons = [];
+    querySnapshot.forEach(function(doc) {
+      useds.push(doc.data().used);
+			created_ats.push(doc.data().created_at)
+			currentbalances.push(doc.data().currentbalance)
+			reasons.push(doc.data().reason)
+    });
+    var toprint = currentbalances.join(", ");
+		console.log(reasons)
+		console.log(useds)
+		console.log(currentbalances)
+		console.log(created_ats)
+		usagelogadder(useds, reasons, created_ats)
+		balanceupdater(currentbalances)
+  });
