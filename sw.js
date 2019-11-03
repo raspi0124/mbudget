bbc53c9f9312455d2844238130d79ad7ab7bdd72
raspi0124 <adores0124@yahoo.co.jp>
2019-11-04 03:01:02 +0900

    Added planty of urls to serviceworker, deleted image files

--- sw.js
+++ sw.js
@@ -1,9 +1,21 @@
 const STATIC_DATA = [
   'index.html',
 	'addusage.html',
-  '/assets/*',
+  '/assets/bootstrap/css/bootstrap.min.css',
+	'/assets/js/theme.js',
   'addusagelib.js',
-  'clientlib.js'
+  'clientlib.js',
+	'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
+	'https://cdn.jsdelivr.net/gh/yukik/koyomi/public/koyomi.min.js',
+	'https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css',
+	'assets/style.css',
+	'https://www.gstatic.com/firebasejs/7.2.3/firebase-firestore.js',
+	'https://www.gstatic.com/firebasejs/7.2.3/firebase-app.js',
+	'https://www.gstatic.com/firebasejs/7.2.3/firebase-analytics.js',
+	'https://cdn.jsdelivr.net/npm/toastify-js',
+	'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js',
+	'https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.js',
+	'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js'
 ];
 
 self.addEventListener('install', function(e) {
